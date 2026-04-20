ca1b913f58d697cbe2ebedaac1f0e6f2cbf8986c
Felix <felix@msynthnova.com>
2026-04-20 16:06:51 -0500

    clean up

--- init.lua
+++ init.lua
@@ -1,54 +1,13 @@
-vim.opt.scrolloff = 10
-vim.opt.sidescrolloff = 10
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.number = true
 vim.opt.wrap = false
 vim.opt.relativenumber = true
-vim.opt.termguicolors = true
 vim.opt.clipboard = "unnamedplus"
-vim.opt.signcolumn = "yes"
-vim.opt.background = "dark"
-vim.opt.pumheight = 5 -- limit completion items
-vim.g.mapleader = " "
 
 local opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
 
-keymap("v", ">", ">gv^", opts)
-keymap("v", "<", "<gv^", opts)
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
 keymap("n", "<S-h>", "<CMD>bprevious<CR>", opts)
 keymap("n", "<S-l>", "<CMD>bnext<CR>", opts)
-
--- Define highlight groups for different modes
-vim.api.nvim_set_hl(0, "StatusLineNormal", { fg = "#ffffff", bg = "#005f5f" })
-vim.api.nvim_set_hl(0, "StatusLineInsert", { fg = "#ffffff", bg = "#5f00af" })
-
--- Set initial color (normal mode)
-vim.api.nvim_set_hl(0, "StatusLine", { fg = "#ffffff", bg = "#005f5f" })
-
--- Autocommands to change status line color based on mode
-vim.api.nvim_create_augroup("ModeColor", { clear = true })
-
-vim.api.nvim_create_autocmd("InsertEnter", {
-    group = "ModeColor",
-    pattern = "*",
-    callback = function()
-        vim.api.nvim_set_hl(0, "StatusLine", { fg = "#ffffff", bg = "#5f00af" })
-    end,
-})
-
-vim.api.nvim_create_autocmd("InsertLeave", {
-    group = "ModeColor",
-    pattern = "*",
-    callback = function()
-        vim.api.nvim_set_hl(0, "StatusLine", { fg = "#ffffff", bg = "#005f5f" })
-    end,
-})
-
-vim.cmd("syntax off")
--- zshrc
+++ zshrc
@@ -1,4 +1,2 @@
-PS1="%F{blue}%~%f %F{red}---------->%f "
-set -o vi
 PS1="%F{yellow}%*%f %F{red}%n@%m%f %F{green}%~%f %# "
-
+set -o vi
